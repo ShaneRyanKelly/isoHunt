5be76beb80da04f6c7294aa6347f4c695930145e
Shane Kelly <shaneryankelly@gmail.com>
2023-02-09 18:33:38 -0500

    the velocity problem
    modified movement to use rigidbody velocity

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     public float moveDistance = 0f;
     public float diagonalMove;
     public Camera mainCamera;
+    public Rigidbody body;
     float xVector, yVector;
 
     // Start is called before the first frame update
@@ -25,89 +26,89 @@ public class Player : MonoBehaviour
     void HandleMovement(){
         xVector = Mathf.Round(Input.GetAxisRaw("Horizontal"));
         yVector = Mathf.Round(Input.GetAxisRaw("Vertical"));
-        Debug.Log("x: " + xVector + " y: " + yVector);
+        //Debug.Log("x: " + xVector + " y: " + yVector);
         if (Mathf.Abs(xVector) == Mathf.Abs(yVector)){
             
         }
         if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 180){
             if (yVector == 1)
-                transform.position += moveDistance * new Vector3(0f,0f,-1);
+                body.velocity += moveDistance * new Vector3(0f,0f,-1);
             else if (yVector == -1)
-                transform.position += moveDistance * new Vector3(0f,0f,1);
+                body.velocity += moveDistance * new Vector3(0f,0f,1);
             else if (xVector == 1)
-                transform.position += moveDistance * new Vector3(-1,0f,0f);
+                body.velocity += moveDistance * new Vector3(-1,0f,0f);
             else if(xVector == -1)
-                transform.position += moveDistance * new Vector3(1,0f,0f);
+                body.velocity += moveDistance * new Vector3(1,0f,0f);
         }
         else if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 90){
             if (yVector == 1)
-                transform.position += new Vector3(moveDistance,0f,0f);
+                body.velocity += moveDistance * new Vector3(1,0f,0f);
             if (yVector == -1)
-                transform.position += new Vector3(-moveDistance,0f,0f);
+                body.velocity += moveDistance * new Vector3(-1,0f,0f);
             if (xVector == 1)
-                transform.position += new Vector3(0f,0f,-moveDistance);
+                body.velocity += moveDistance * new Vector3(0f,0f,-1);
                 if (xVector == -1)
-                transform.position += new Vector3(0f,0f,moveDistance);
+                body.velocity += moveDistance * new Vector3(0f,0f,1);
         }
         else if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 270){
             if (yVector == 1)
-                transform.position += new Vector3(-moveDistance,0f,0f);
+                body.velocity += moveDistance * new Vector3(-1,0f,0f);
             if (yVector == -1)
-                transform.position += new Vector3(moveDistance,0f,0f);
+                body.velocity += moveDistance * new Vector3(1,0f,0f);
             if (xVector == 1)
-                transform.position += new Vector3(0f,0f,moveDistance);
+                body.velocity += moveDistance * new Vector3(0f,0f,1);
             if (xVector == -1)
-                transform.position += new Vector3(0f,0f,-moveDistance);
+                body.velocity += moveDistance * new Vector3(0f,0f,-1);
         }
         else if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 45){
             if (yVector == 1)
-                transform.position += new Vector3(diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,1);
             if (yVector == -1)
-                transform.position += new Vector3(-diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,-1);
             if (xVector == 1)
-                transform.position += new Vector3(diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,-1);
             if (xVector == -1)
-                transform.position += new Vector3(-diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,1);
         }
         else if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 135){
             if (yVector == 1)
-                transform.position += new Vector3(diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,-1);
             if (yVector == -1)
-                transform.position += new Vector3(-diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,1);
             if (xVector == 1)
-                transform.position += new Vector3(-diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,-1);
             if (xVector == -1)
-                transform.position += new Vector3(diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,1);
         }
         else if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 225){
             if (yVector == 1)
-                transform.position += new Vector3(-diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,-1);
             if (yVector == -1)
-                transform.position += new Vector3(diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,1);
             if (xVector == 1)
-                transform.position += new Vector3(-diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,1);
             if (xVector == -1)
-                transform.position += new Vector3(diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,-1);
         }
         else if (Mathf.Round(mainCamera.transform.eulerAngles.y) == 315){
             if (yVector == 1)
-                transform.position += new Vector3(-diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,1);
             if (yVector == -1)
-                transform.position += new Vector3(diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,-1);
             if (xVector == 1)
-                transform.position += new Vector3(diagonalMove,0f,diagonalMove);
+                body.velocity += moveDistance * new Vector3(1,0f,1);
             if (xVector == -1)
-                transform.position += new Vector3(-diagonalMove,0f,-diagonalMove);
+                body.velocity += moveDistance * new Vector3(-1,0f,-1);
         }
         else{
             if (yVector == 1)
-                transform.position += new Vector3(0f,0f,moveDistance);
+                body.velocity += moveDistance * new Vector3(0f,0f,1);
             if (yVector == -1)
-                transform.position += new Vector3(0f,0f,-moveDistance);
+                body.velocity += moveDistance * new Vector3(0f,0f,-1);
             if (xVector == 1)
-                transform.position += new Vector3(moveDistance,0f,0f);
+                body.velocity += moveDistance * new Vector3(1,0f,0f);
             if (xVector == -1)
-                transform.position += new Vector3(-moveDistance,0f,0f);
+                body.velocity += moveDistance * new Vector3(-1,0f,0f);
         }
         /*if (Mathf.Round(mainCamera.transform.eulerAngles.y) > 90f && Mathf.Round(mainCamera.transform.eulerAngles.y) < 270f){
             if (Input.GetKeyDown("w"))
@@ -149,5 +150,8 @@ public class Player : MonoBehaviour
             if (Input.GetKeyDown("d"))
                 transform.position += new Vector3(moveDistance,0f,0f);
         }*/
+        void OnCollisionEnter(Collision other) {
+            Debug.Log("collision detected");
+        }
     }
 }
